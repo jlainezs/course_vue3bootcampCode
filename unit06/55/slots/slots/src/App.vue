dd0120eafec372141c062de37305b099a7947214
Pep Lainez <jlainezs@gmail.com>
2024-08-05 23:36:37 +0200

    68 starting with slots.

---
+++ unit06/55/slots/slots/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <app-form>
+    <div class="help">
+        <p>THis is some help text</p>
+      </div>
+      <div class="fields">
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="password" />
+      </div>
+      <div class="buttons">
+        <button type="submit">Submit</button>
+      </div>
+  </app-form>
+  <app-form></app-form>
+</template>
+
+<script>
+import AppForm from "./components/Form.vue"
+
+export default {
+  name: "App",
+  components: {
+    AppForm:AppForm
+  }
+}
+</script>
+
