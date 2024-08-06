786cb30f9f7c81897b493e1e4d8c0f5133ef356c
Pep Lainez <jlainezs@gmail.com>
2024-08-06 10:08:33 +0200

    70 Dynamic components.

---
+++ unit07/70/dynamic-components/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Home page</h1>
+</template>
+<script>
+export default {
+    unmounted(){
+        console.log("Unmounted home")
+    },
+    activated(){
+        console.log("Activated home")
+    },
+    deactivated(){
+        console.log("Deativated home")
+    }
+}
+</script>
