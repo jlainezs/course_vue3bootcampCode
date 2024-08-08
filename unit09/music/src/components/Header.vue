e7628ae8496e2766877b5c0892a113ba07737920
Pep Lainez <jlainezs@gmail.com>
2024-08-08 16:40:11 +0200

    98 Create a state property

--- unit09/music/src/components/Header.vue
+++ unit09/music/src/components/Header.vue
@@ -9,7 +9,7 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
+            <a class="px-2 text-white" @click.prevent="toggleAuthModal" href="#">Login / Register</a>
           </li>
           <li>
             <a class="px-2 text-white" href="#">Manage</a>
@@ -19,3 +19,21 @@
     </nav>
   </header>
 </template>
+
+<script>
+import { mapStores } from "pinia";
+import useModalStore from "@/stores/modal";
+
+export default {
+  name: "AppHeader",
+  computed: {
+    ...mapStores(useModalStore),
+  },
+  methods: {
+    toggleAuthModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen;
+      console.log(this.modalStore.isOpen);
+    }
+  }
+}
+</script>
