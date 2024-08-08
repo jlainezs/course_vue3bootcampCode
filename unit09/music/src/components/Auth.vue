f47187ce654607b9a031fa6cee6bd8fdd58f316b
Pep Lainez <jlainezs@gmail.com>
2024-08-08 20:22:24 +0200

    115 submit form validation

--- unit09/music/src/components/Auth.vue
+++ unit09/music/src/components/Auth.vue
@@ -77,7 +77,8 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema"
+            @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -186,6 +187,11 @@ export default {
         tos: "required",
       }
     }
+  },
+  methods: {
+    register(values) {
+      console.log(values);
+    },
   },
   computed: {
     ...mapState(useModalStore, ['hiddenClass']),
