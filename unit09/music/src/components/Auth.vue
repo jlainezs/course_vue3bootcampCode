5c5e190584ef6e3f09bfa9e26e9085c378cafe2f
Pep Lainez <jlainezs@gmail.com>
2024-08-09 16:29:18 +0200

    121 Showing alerts.

--- unit09/music/src/components/Auth.vue
+++ unit09/music/src/components/Auth.vue
@@ -76,7 +76,14 @@
               Submit
             </button>
           </form>
+
           <!-- Registration Form -->
+          <div class="text-white text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+          {{ reg_alert_msg }}
+          </div>
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
             :initial-values="userData"
             @submit="register">
@@ -164,6 +171,7 @@
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="reg_in_submission"
             >
               Submit
             </button>
@@ -187,18 +195,29 @@ export default {
         name: "required|min:3|max:100|alpha_spaces",
         email: "required|email|max:100",
         age: "required|min_value:18|max_value:100",
-        password: "required|min:9|max:100|excluded:password",
+        password: "required|min:5|max:100|excluded:password",
         confirm_password: "passwords_mismatch:@password",
         country: "required|country_excluded:XXX",
         tos: "tos",
       },
       userData: {
         country: 'USA',
-      }
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: "bg-blue-500",
+      reg_alert_msg: "Please wait! Your account is being created.",
     }
   },
   methods: {
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = "bg-blue-500";
+      this.reg_alert_msg = "Please wait! Your account is being created.";
+
+      this.reg_alert_variant = "bg-green-500";
+      this.reg_alert_msg = "Success! Your account has been created.";
       console.log(values);
     },
   },
