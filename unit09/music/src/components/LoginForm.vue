9ad6b0287e4962be50cbea5b8a2ee761e881296e
Pep Lainez <jlainezs@gmail.com>
2024-08-10 19:32:41 +0200

    138 Setting up the login

--- unit09/music/src/components/LoginForm.vue
+++ unit09/music/src/components/LoginForm.vue
@@ -37,6 +37,9 @@
 
 </template>
 <script>
+import { mapActions } from 'pinia';
+import useUserStore from "@/stores/user";
+
 export default {
     name: 'LoginForm',
     data() {
@@ -50,19 +53,30 @@ export default {
             login_alert_variant: "bg-blue-500",
             login_alert_msg: "Please wait! We are logging you in.",
         }
-  },
-  methods: {
-    login(values) {
-          this.login_in_submission = true;
-          this.login_show_alert = true;
-          this.login_alert_variant = "bg-blue-500";
-          this.login_alert = "Please wait! We are logging you in.";
+    },
+    methods: {
+        ...mapActions(useUserStore, ["authenticate"]),
+        async login(values) {
+            this.login_in_submission = true;
+            this.login_show_alert = true;
+            this.login_alert_variant = "bg-blue-500";
+            this.login_alert = "Please wait! We are logging you in.";
 
-          this.login_alert_variant = "bg-green-500";
-          this.login_alert_msg = "Success! You are now logged in.";
+            try {
+                await this.authenticate(values);
+            } catch (error) {
+                this.login_in_submission = false;
+                this.login_alert_variant = "bg-red-500";
+                this.login_alert_msg = "Invalid login details.";
 
-          console.log(values);
-    }
-},
+                return;
+            }
+
+            this.login_alert_variant = "bg-green-500";
+            this.login_alert_msg = "Success! You are now logged in.";
+
+            console.log(values);
+        }
+    },
 }
 </script>
--- unit09/music/src/stores/user.js
+++ unit09/music/src/stores/user.js
@@ -22,6 +22,10 @@ export default defineStore("user", {
             });
 
             this.userLoggedIn = true;
+        },
+        async authenticate(values) {
+            await auth.signInWithEmailAndPassword(values.email, values.password);
+            this.userLoggedIn = true;
         }
     },
 });
