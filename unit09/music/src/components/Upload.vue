04ebae60043a224eca4a9406d6703d61b2c760ad
Pep Lainez <jlainezs@gmail.com>
2024-08-11 10:12:07 +0200

    157 Catch drag and drop events.

--- unit09/music/src/components/Upload.vue
+++ unit09/music/src/components/Upload.vue
@@ -5,14 +5,23 @@
         <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
+
         <!-- Upload Dropbox -->
         <div
             class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
-        >
+            :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
+            @drag.prevent.stop=""
+            @dragstart.prevent.stop=""
+            @dragend.prevent.stop="is_dragover = false"
+            @dragover.prevent.stop="is_dragover = true"
+            @dragenter.prevent.stop="is_dragover = true"
+            @dragleave.prevent.stop="is_dragover = false"
+            @drop.prevent.stop="upload">
             <h5>Drop your files here</h5>
         </div>
         <hr class="my-6" />
-        <!-- Progess Bars -->
+
+        <!-- Progress Bars -->
         <div class="mb-4">
             <!-- File Name -->
             <div class="font-bold text-sm">Just another song.mp3</div>
@@ -40,5 +49,15 @@
 <script>
 export default {
     name: "Upload",
+    data() {
+        return {
+            is_dragover: false,
+        }
+    },
+    methods: {
+        upload() {
+            this.is_dragover = false;
+        },
+    }
 }
 </script>
