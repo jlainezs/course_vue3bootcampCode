954c6a54b02f463a431441252e649bcdb393bd85
Pep Lainez <jlainezs@gmail.com>
2024-08-12 18:07:15 +0200

    215 Changing locales

--- unit09/music/src/components/Header.vue
+++ unit09/music/src/components/Header.vue
@@ -26,6 +26,14 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#"
+              @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -40,6 +48,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "ca" ? "Català" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -52,7 +63,10 @@ export default {
       if (this.$route.mata.requiresAuth) {
         this.$router.push({ name: 'home' });
       }
-    }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "ca" ? "en" : "ca";
+    },
   }
 }
 </script>
