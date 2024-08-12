10a7bb6c8b904087a59bf132dc180fec443d5a6d
Pep Lainez <jlainezs@gmail.com>
2024-08-12 11:06:24 +0200

    190 Loading song comments.

--- unit09/music/src/views/Song.vue
+++ unit09/music/src/views/Song.vue
@@ -59,80 +59,17 @@
     </section>
     <!-- Comments -->
     <ul class="container mx-auto">
-      <li class="p-6 bg-gray-50 border border-gray-200">
+      <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in comments" :key="comment.docID">
         <!-- Comment Author -->
         <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
+          <div class="font-bold">{{ comment.name }}</div>
+          <time>{{ comment.datePosted }}</time>
         </div>
-
         <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
-        </p>
-      </li>
-      <li class="p-6 bg-gray-50 border border-gray-200">
-        <!-- Comment Author -->
-        <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
-        </div>
-
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
-        </p>
-      </li>
-      <li class="p-6 bg-gray-50 border border-gray-200">
-        <!-- Comment Author -->
-        <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
-        </div>
-
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
-        </p>
-      </li>
-      <li class="p-6 bg-gray-50 border border-gray-200">
-        <!-- Comment Author -->
-        <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
-        </div>
-
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
-        </p>
-      </li>
-      <li class="p-6 bg-gray-50 border border-gray-200">
-        <!-- Comment Author -->
-        <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
-        </div>
-
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
-        </p>
-      </li>
-      <li class="p-6 bg-gray-50 border border-gray-200">
-        <!-- Comment Author -->
-        <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
-        </div>
-
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
+          {{ comment.content }}
         </p>
       </li>
     </ul>
-
 </template>
 
 <script>
@@ -148,6 +85,7 @@ export default {
       schema: {
         comment: 'required|min:3',
       },
+      comments: [],
       comment_in_submission: false,
       comment_show_alert: false,
       comment_alert_variant: "bg-blue-500",
@@ -177,7 +115,17 @@ export default {
       this.comment_alert_variant = "bg-green-500";
       this.comment_alert_message = "Comment added!";
       resetForm();
+    },
+    async getComments() {
+      const snapshots = await commentsCollection.where('sid', '==', this.$route.params.id).get();
+      this.comments = []; // clean up previous comments.
 
+      snapshots.forEach((document) => {
+        this.comments.push({
+          docID: document.id,
+          ...document.data(),
+        });
+      });
     },
   },
   async created() {
@@ -189,6 +137,7 @@ export default {
     }
 
     this.song = docSnapshot.data();
+    this.getComments();
   }
 }
 </script>
