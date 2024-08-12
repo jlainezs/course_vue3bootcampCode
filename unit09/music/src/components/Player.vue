03754a1c6c6682ed848a9253c1d38451f42fb499
Pep Lainez <jlainezs@gmail.com>
2024-08-12 12:49:53 +0200

    199 Time formatting.

--- unit09/music/src/components/Player.vue
+++ unit09/music/src/components/Player.vue
@@ -13,7 +13,7 @@
                     :class="{'fa-play': !playing, 'fa-pause': playing}"></i>
             </button>
             <!-- Current Position -->
-            <div class="player-currenttime">00:00</div>
+            <div class="player-currenttime">{{ seek }}</div>
             <!-- Scrub Container  -->
             <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
                 <!-- Player Ball -->
@@ -30,7 +30,7 @@
                 ></span>
             </div>
             <!-- Duration -->
-            <div class="player-duration">03:06</div>
+            <div class="player-duration">{{ duration }}</div>
         </div>
     </div>
 
@@ -45,7 +45,7 @@ export default {
         ...mapActions(usePlayerStore, ["toggleAudio"]),
     },
     computed: {
-        ...mapState(usePlayerStore, ["playing"]),
+        ...mapState(usePlayerStore, ["playing", "duration", "seek"]),
     }
 }
 </script>
--- unit09/music/src/stores/player.js
+++ unit09/music/src/stores/player.js
@@ -1,10 +1,13 @@
 import { defineStore } from "pinia";
 import { Howl } from "howler";
+import helper from "@/includes/helper";
 
 export default defineStore("player", {
     state: () => ({
         current_song: {},
         sound: {},
+        seek: "00:00",
+        duration: "00:00",
     }),
     actions: {
         async newSong(song) {
@@ -15,6 +18,10 @@ export default defineStore("player", {
                 html5: true,
             });
             this.sound.play();
+
+            this.sound.on("play", () => {
+                requestAnimationFrame(this.progress);
+            });
         },
         async toggleAudio() {
             if (!this.sound.playing) {
@@ -26,6 +33,14 @@ export default defineStore("player", {
             } else {
                 this.sound.play();
             }
+        },
+        progress() {
+            this.seek = helper.formatTime(this.sound.seek());
+            this.duration = helper.formatTime(this.sound.duration());
+
+            if (this.sound.playing()){
+                requestAnimationFrame(this.progress);
+            };
         },
     },
     getters: {
