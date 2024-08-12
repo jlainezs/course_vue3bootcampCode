3eb1d2462f494d297809126732f35c4102528870
Pep Lainez <jlainezs@gmail.com>
2024-08-12 17:34:35 +0200

    211 Translate content with i18n

--- unit09/music/src/includes/i18n.js
+++ unit09/music/src/includes/i18n.js
@@ -1,10 +1,11 @@
 import { createI18n } from "vue-i18n";
 import en from "@/locales/en.json";
+import ca from "@/locales/ca.json";
 
 export default createI18n({
     locale: "en",
     fallbackLocale: "en",
     messages: {
-        en,
+        en, ca
     },
 });
