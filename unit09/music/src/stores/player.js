59d832dbf7026f8e40374008bda23fabe3355a81
Pep Lainez <jlainezs@gmail.com>
2024-08-12 13:20:45 +0200

    201 Player progress bar and change player position.

--- unit09/music/src/stores/player.js
+++ unit09/music/src/stores/player.js
@@ -8,9 +8,15 @@ export default defineStore("player", {
         sound: {},
         seek: "00:00",
         duration: "00:00",
+        playerProgress: "0%",
     }),
     actions: {
         async newSong(song) {
+            if (this.sound instanceof Howl)
+            {
+                this.sound.unload();
+            }
+
             this.current_song = song;
 
             this.sound = new Howl({
@@ -38,9 +44,24 @@ export default defineStore("player", {
             this.seek = helper.formatTime(this.sound.seek());
             this.duration = helper.formatTime(this.sound.duration());
 
+            this.playerProgress = `${(this.sound.seek() / this.sound.duration()) * 100}%`
+
             if (this.sound.playing()){
                 requestAnimationFrame(this.progress);
             };
+        },
+        updateSeek(event) {
+            if (!this.sound.playing) {
+                return;
+            }
+
+            const { x, width } = event.currentTarget.getBoundingClientRect();
+            const clickX = event.clientX - x;
+            const percentage = clickX / width;
+            const seconds = this.sound.duration() * percentage;
+
+            this.sound.seek(seconds);
+            this.sound.once("seek", this.progress);
         },
     },
     getters: {
