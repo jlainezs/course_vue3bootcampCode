5ca666b8e448878705c9bfdc7ddda5f2b8252555
Pep Lainez <jlainezs@gmail.com>
2024-08-13 10:21:00 +0200

    221 Configure PWA plugin and.

--- unit09/music/src/main.js
+++ unit09/music/src/main.js
@@ -8,6 +8,9 @@ import { auth } from "./includes/firebase";
 
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
+import {registerSW} from "virtual:pwa-register";
+
+registerSW({immediate: true});
 
 import "./assets/base.css"
 import "./assets/main.css"
--- unit09/music/vite.config.js
+++ unit09/music/vite.config.js
@@ -12,7 +12,24 @@ export default defineConfig({
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
-      }
+      },
+      manifest: {
+        name: "Music App",
+        short_name: "Music App",
+        description: "Vue sample PWA",
+        theme_color: "#ff5e3a",
+        icons: [
+          {
+            src: "assets/img/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/img/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+      },
     }),
   ],
   resolve: {
