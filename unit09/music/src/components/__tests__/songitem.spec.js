0d545f0369495be33dffbb4ecc718731884c1324
Pep Lainez <jlainezs@gmail.com>
2024-08-13 19:29:31 +0200

    246 Test attributes.

--- unit09/music/src/components/__tests__/songitem.spec.js
+++ unit09/music/src/components/__tests__/songitem.spec.js
@@ -4,7 +4,7 @@ import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 describe("SongItem.vue", () => {
     test('render song.display_name', () => {
         const song = {
-            display_name : test,
+            display_name : "test",
         };
         const wrapper = shallowMount(SongItem, {
             propsData: {
@@ -18,5 +18,23 @@ describe("SongItem.vue", () => {
         });
         const compositionAuthor = wrapper.find(".text-gray-500");
         expect(compositionAuthor.text()).toContain(song.display_name);
+    });
+
+    test('render song.docID in id attribute', () => {
+        const song = {
+            docID: "abc",
+        };
+        const wrapper = shallowMount(SongItem, {
+            propsData: {
+                song,
+            },
+            global: {
+                components: {
+                    'router-link': RouterLinkStub,
+                },
+            },
+        });
+
+        expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
     });
 });
